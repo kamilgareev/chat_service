e4322693c46ed0ef9fda2b0ab91a5b654b27aa2b
kamilgareev <kamalearningandbusiness@gmail.com>
2025-03-30 20:06:48 +0700

    added DI container

---
+++ app/containers.py
@@ -0,0 +1,34 @@
+from dependency_injector import containers, providers
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+
+from config.settings import settings
+from repositories import ChatRepository
+from services import ChatService
+
+
+class Container(containers.DeclarativeContainer):
+    config = providers.Configuration()
+
+    db_engine = providers.Singleton(
+        create_async_engine,
+        settings.db.DB_URL
+    )
+
+    db_session = providers.Factory(
+        async_sessionmaker,
+        bind=db_engine,
+        class_=AsyncSession,
+        expire_on_commit=False
+    )
+
+    chat_repository = providers.Factory(
+        ChatRepository
+    )
+
+    chat_service = providers.Factory(
+        ChatService,
+        chat_repository=chat_repository
+    )
+
+
+
