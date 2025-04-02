c5e973191f044429062ab8a6b4535b41a1d59982
kamilgareev <kamalearningandbusiness@gmail.com>
2025-04-02 12:47:12 +0700

    WS manager method changes

--- app/core/managers.py
+++ app/core/managers.py
@@ -1,5 +1,5 @@
 from collections import defaultdict
-from typing import Dict, Set
+from typing import Dict, Set, Any
 
 from fastapi import WebSocket
 
@@ -8,15 +8,15 @@ class WebsocketManager:
     def __init__(self):
         self.active_connections: Dict[int, Set[WebSocket]] = defaultdict(set)
 
-    async def connect(self, chat_id: int, websocket: WebSocket):
+    async def connect(self, chat_id: int, websocket: WebSocket) -> None:
         await websocket.accept()
         self.active_connections[chat_id].add(websocket)
 
-    async def disconnect(self, chat_id: int, websocket: WebSocket):
+    async def disconnect(self, chat_id: int, websocket: WebSocket) -> None:
         self.active_connections[chat_id].remove(websocket)
         if not self.active_connections[chat_id]:
             del self.active_connections[chat_id]
 
-    async def send_message(self, chat_id: int, message: str):
+    async def send_message(self, chat_id: int, message_data: dict[str, Any]) -> None:
         for connection in self.active_connections[chat_id]:
-            await connection.send_json(message)
+            await connection.send_json(message_data)
